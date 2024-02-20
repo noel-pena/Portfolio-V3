087060a5a2bcac32163f481a6bc3a0dfe0c2c03b
noel-pena <azulu88@hotmail.com>
2024-02-20 13:44:19 -0500

    more designing, completed header and added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Home />
+      <About />
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,76 @@
-import React from "react";
-import { AppBar, Toolbar, IconButton, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "@mui/material";
+import { animateScroll as scroll } from "react-scroll";
 
 export const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit" component="div">
-          Navbar
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <nav
+      className="nav-items"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "1rem",
+        gap: "5px",
+      }}
+    >
+      <Link
+        component="button"
+        className="nav-item"
+        underline="none"
+        to=""
+        smooth={true}
+        duration={500}
+        offset={-50}
+        sx={{ fontWeight: 100 }}
+      >
+        Home
+      </Link>
+      <Link
+        className="nav-item"
+        component="button"
+        underline="none"
+        to=""
+        smooth={true}
+        duration={500}
+        offset={-50}
+        sx={{ fontWeight: 100 }}
+      >
+        About
+      </Link>
+      <Link
+        className="nav-item"
+        component="button"
+        underline="none"
+        to=""
+        smooth={true}
+        duration={500}
+        offset={-50}
+        sx={{ fontWeight: 100 }}
+      >
+        Projects
+      </Link>
+      <Link
+        className="nav-item"
+        component="button"
+        underline="none"
+        to=""
+        smooth={true}
+        duration={500}
+        offset={-50}
+        sx={{ fontWeight: 100 }}
+      >
+        Music
+      </Link>
+      <Link
+        className="nav-item"
+        component="button"
+        href="https://cdn.noel-pena.com/Resume.pdf"
+        underline="none"
+        sx={{ fontWeight: 100 }}
+      >
+        Resume
+      </Link>
+    </nav>
   );
 };
