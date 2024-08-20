6f1b7c5ec35c44f84e34c9a163623f00c842684b
noel-pena <azulu88@hotmail.com>
2024-08-19 21:15:15 -0400

    attempting to create a feature where the user scrolls and more of the page loads.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Container, CssBaseline, ThemeProvider } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, Box } from "@mui/material";
 import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 import { About } from "./components/About";
@@ -10,21 +10,52 @@ import { Music } from "./components/Music";
 import { Footer } from "./components/Footer";
 import { Experience } from "./components/Experience";
 import { theme } from "./components/sub-component/theme";
+import { useEffect, useRef, useState } from "react";
 
 export const App = () => {
+  const [loadComponents, setLoadComponents] = useState(false);
+  const homeRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting === false) {
+          setLoadComponents(true);
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (homeRef.current) {
+      observer.observe(homeRef.current);
+    }
+
+    return () => {
+      if (homeRef.current) {
+        observer.unobserve(homeRef.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <Container>
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Header />
-          <Home />
-          <About />
-          <Skills />
-          <Projects />
-          <Experience />
-          <Pictures />
-          <Music />
+          <Box ref={homeRef} sx={{ minHeight: "100vh" }}>
+            {!loadComponents && <Home />}
+          </Box>
+          {loadComponents && (
+            <Box>
+              <About />
+              <Skills />
+              <Projects />
+              <Experience />
+              <Pictures />
+              <Music />
+            </Box>
+          )}
           <Footer />
         </ThemeProvider>
       </Container>
