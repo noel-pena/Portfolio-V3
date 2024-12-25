b222bc6d2b583aa9287183c7d1f7a7bfd466dfa3
noel-pena <azulu88@hotmail.com>
2024-12-25 13:13:15 -0500

    styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,4 @@
-import { Grid2 as Grid } from "@mui/material";
+import {Grid2 as Grid, Typography} from "@mui/material";
 
 export const About = () => {
   return (
@@ -17,28 +17,30 @@ export const About = () => {
           lineHeight: 1.5,
         }}
       >
-        I am a dynamic Full-Stack Developer with a keen eye for design and a
-        solid command of HTML, CSS, and JavaScript. Proven history of creating
-        visually appealing and user-friendly websites by translating design
-        concepts into seamless, responsive interfaces. Adept at collaborating
-        with cross-functional teams to deliver high-quality web solutions that
-        enhance user experiences and align with business objectives. I code
-        mainly in React.js. On my free time, I am a photographer and also do a
-        lot of photo editing. I am a music producer on the side where I
-        primarily work with virtual and live instruments. If you are interested
-        in my work, please feel free to contact me. You may also download a copy
-        of my resume through my cdn
-        <a
-          href="https://cdn.noel-pena.com/Noel-Pena.pdf"
-          style={{
-            color: "#4c5c9e",
-            textDecoration: "none",
-            fontWeight: "500",
-          }}
-        >
-          {" "}
-          here.
-        </a>
+        <Typography>
+            I am a dynamic Full-Stack Developer with a keen eye for design and a
+            solid command of HTML, CSS, and JavaScript. Proven history of creating
+            visually appealing and user-friendly websites by translating design
+            concepts into seamless, responsive interfaces. Adept at collaborating
+            with cross-functional teams to deliver high-quality web solutions that
+            enhance user experiences and align with business objectives. I code
+            mainly in React.js. On my free time, I am a photographer and also do a
+            lot of photo editing. I am a music producer on the side where I
+            primarily work with virtual and live instruments. If you are interested
+            in my work, please feel free to contact me. You may also download a copy
+            of my resume through my cdn
+            <a
+              href="https://cdn.noel-pena.com/Noel-Pena.pdf"
+              style={{
+                color: "#4c5c9e",
+                textDecoration: "none",
+                fontWeight: "500",
+              }}
+            >
+              {" "}
+              here.
+            </a>
+          </Typography>
       </Grid>
     </Grid>
   );
